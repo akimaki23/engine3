0678a100fd2bbea8cce5ee49692223b13939f2f7
Victor Popovici <victorpopovici@gmail.com>
2016-03-19 12:07:31 +0100

    [fixed] baseclient rollover on ack for edge cases

--- MMOEngine/src/engine/service/proto/BaseClient.cpp
+++ MMOEngine/src/engine/service/proto/BaseClient.cpp
@@ -869,17 +869,12 @@ void BaseClient::acknowledgeServerPackets(uint16 seq) {
 		}
 
 		int32 realseq = seq;
-		if (realseq < acknowledgedServerSequence) {
-			realseq = (seq & 0xFFFF);
+		if (seq < acknowledgedServerSequence) {
+			realseq = (seq & 0xFFFF) | (serverSequence & 0xFFFF0000);
+		}
 
-			uint16 lastAckedOverflow = (uint16) ((acknowledgedServerSequence & 0xFFFF0000) >> 16);
-			uint16 lastAckedSeqRaw = (uint16) (acknowledgedServerSequence & 0x0000FFFF);
-
-			if (realseq < lastAckedSeqRaw) {
-				realseq = (lastAckedOverflow + 1) << 16 | realseq;
-			} else {
-				realseq = (lastAckedOverflow) << 16 | realseq;
-			}
+		if ((uint31)realseq > serverSequence) {
+			realseq -= 0x10000;
 		}
 
 		#ifdef TRACE_CLIENTS
