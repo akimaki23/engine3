7d68f7e911f932fbbaddbf6f82ce87d0fd0f0316
Victor Popovici <victorpopovici@gmail.com>
2017-06-25 16:58:18 +0200

    [fixed] typo #2

--- MMOEngine/src/engine/service/proto/BaseClient.cpp
+++ MMOEngine/src/engine/service/proto/BaseClient.cpp
@@ -148,7 +148,7 @@ BaseClient::~BaseClient() {
 		ServiceClient::close();
 
 #ifdef LOCKFREE_BCLIENT_BUFFERS
-	delete sendUnreliableBuffer;
+	delete sendLockFreeBuffer;
 	sendLockFreeBuffer = NULL;
 #endif
 
