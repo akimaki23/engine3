ff9b7b4ba7e8cf424680a7d9a187eec135b97d8a
Victor Popovici <victorpopovici@gmail.com>
2017-08-20 01:18:54 +0200

    [fixed] cond mutex unlock

--- MMOEngine/src/engine/core/TimedTaskQueue.cpp
+++ MMOEngine/src/engine/core/TimedTaskQueue.cpp
@@ -225,6 +225,8 @@ Task* TimedTaskQueue::get() {
 				int64 difference = -task->getNextExecutionTime().miliDifference();
 
 				if (difference > 10) {
+					condMutex->unlock();
+
 					return NULL;
 				}
 			}
